c818d05f5e5d105ed26f642c87957abba0c87b60
xychenxy <chenxinyi1978@hotmail.com>
2022-12-08 11:21:04 +0800

    Update fuseutil.go

--- fuseutil/fuseutil.go
+++ fuseutil/fuseutil.go
@@ -1,7 +1,7 @@
 package fuseutil // import "bazil.org/fuse/fuseutil"
 
 import (
-	"bazil.org/fuse"
+	"github.com/simonchen/fuse"
 )
 
 // HandleRead handles a read request assuming that data is the entire file content.
