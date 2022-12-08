245f435d66ca88118ba8cf9c391417cd9eae377e
xychenxy <chenxinyi1978@hotmail.com>
2022-12-08 11:20:30 +0800

    Update serve.go

--- fs/serve.go
+++ fs/serve.go
@@ -1,6 +1,6 @@
 // FUSE service loop, for servers that wish to use it.
 
-package fs // import "bazil.org/fuse/fs"
+package fs // import "github.com/simonchen/fuse/fs"
 
 import (
 	"encoding/binary"
@@ -20,8 +20,8 @@ import (
 import (
 	"bytes"
 
-	"bazil.org/fuse"
-	"bazil.org/fuse/fuseutil"
+	"github.com/simonchen/fuse"
+	"github.com/simonchen/fuse/fuseutil"
 )
 
 const (
